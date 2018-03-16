e31368a560b6568c718281e1284dc8081214c3d1
milad <m.rezai2724@yahoo.com>
2018-03-16 11:31:34 +0330

    commit 3

--- routes/route.py
+++ routes/route.py
@@ -1,4 +1,4 @@
-from Controller import CourseController, LoginController
+from Controller import CourseController, LoginController, ChoiceCourseController, ScheduleController
 from passlib.handlers import bcrypt
 
 
@@ -6,5 +6,13 @@ from passlib.handlers import bcrypt
 def route(api):
     api.add_resource(CourseController.list, '/a')
     api.add_resource(LoginController.Login, '/login')
+    api.add_resource(ChoiceCourseController.List, '/choicecourses', endpoint='choice_course_list')
+    api.add_resource(ChoiceCourseController.Update, '/choicecourses', endpoint='update_choice_courses')
+    api.add_resource(ChoiceCourseController.Delete, '/choicecourses', endpoint='delete_choice_courses')
+    api.add_resource(ChoiceCourseController.Store, '/choicecourse', endpoint='create_choice_course')
+    api.add_resource(ChoiceCourseController.Destroy, '/choicecourses/<int:choice_course_id>',
+                     endpoint='delete_choice_course')
+    api.add_resource(ScheduleController.List, '/schedule', endpoint='schedule')
+
 
 
